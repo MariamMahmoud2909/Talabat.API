6b0764a5fbbaa7629f6e03b9ae792decdbfb49b6
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-04-17 05:13:50 +0200

    Refactor GetProductById Using Specification Design Pattern

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -31,7 +31,8 @@ namespace Talabat.APIs.Controllers
 		[HttpGet("{id}")]
 		public async Task<ActionResult<Product>> GetProduct(int id)
 		{
-			var product = await _productsRepo.GetAsync(id);
+			var spec = new ProductWithBrandAndCategorySpecifications(id);
+			var product = await _productsRepo.GetWithSpecAsync(spec);
 
 			if (product is null)
 				return NotFound(new { StatusCode = 404, Message = "Not Found" }); // Status Code : 404
--- Talabat.Core/Specifications/ProductSpecs/ProductWithBrandAndCategorySpecifications.cs
+++ Talabat.Core/Specifications/ProductSpecs/ProductWithBrandAndCategorySpecifications.cs
@@ -12,7 +12,19 @@ namespace Talabat.Core.Specifications.ProductSpecs
 		public ProductWithBrandAndCategorySpecifications()
 			: base()
 		{
+			AddIncludes();
+		}
+
+		public ProductWithBrandAndCategorySpecifications(int id)
+			: base(P => P.Id == id)
+		{
+			AddIncludes();
+		}
+
+		private void AddIncludes()
+		{
 			Includes.Add(P => P.Brand);
 			Includes.Add(P => P.Category);
 		}
+	}
 }
