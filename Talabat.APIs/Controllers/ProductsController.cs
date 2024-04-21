3630537c1af8192391877aba046ea12960e4a571
Mariam Mahmoud <105247169+MariamMahmoud2909@users.noreply.github.com>
2024-04-21 04:54:25 +0200

    Filteration

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -41,9 +41,9 @@ namespace Talabat.APIs.Controllers
 		}
 
 		[HttpGet]
-		public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+		public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string? sort, int? brandId, int? categoryId)
 		{
-			var spec = new ProductWithBrandAndCategorySpecifications(sort);
+			var spec = new ProductWithBrandAndCategorySpecifications(sort, brandId, categoryId);
 			var products = await _productsRepo.GetAllWithSpecAsync(spec);
 			return Ok(_mapper.Map<IEnumerable<Product>, IEnumerable<ProductToReturnDto>>(products));
 		}
--- Talabat.Core/Specifications/ProductSpecs/ProductWithBrandAndCategorySpecifications.cs
+++ Talabat.Core/Specifications/ProductSpecs/ProductWithBrandAndCategorySpecifications.cs
@@ -9,8 +9,12 @@ namespace Talabat.Core.Specifications.ProductSpecs
 {
 	public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
 	{
-		public ProductWithBrandAndCategorySpecifications(string sort)
-			: base()
+		public ProductWithBrandAndCategorySpecifications(string? sort, int? brandId, int? categoryId) 
+			: base(P =>
+
+				 (!brandId.HasValue || P.BrandId == brandId.Value) &&
+				 (!categoryId.HasValue || P.CategoryId == categoryId.Value)
+				  )
 		{
 			AddIncludes();
 
